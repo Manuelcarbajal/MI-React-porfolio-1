973d13a0902bd3ce23485676a6e70f0271a25bb4
manuelcf <carbajalfloresmanuel@gmail.com>
2022-12-02 16:22:23 -0600

    funciona

--- src/componentes/pages/blog-detail.js
+++ src/componentes/pages/blog-detail.js
@@ -1,64 +1,106 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+
+
+import BlogForm from "../blog/blog-form";
+import BlogFeaturedImage from "../blog/blog-featured-image";
 
 
 class BlogDetail extends Component {
    
-    constructor(props){
+    constructor(props) {
         super(props);
-        
-         
-
+    
         this.state = {
-            currentId : this.props.theParams.slug,
-            blogItem:{}
-        }
-    }
-
-    getBlogItem(){
-        axios.get(`https://manuelll.devcamp.space/portfolio/portfolio_blogs/
-        ${this.state.currentId}`
-        )
-        .then(response => {
-            console.log("getBlogItem error", response);
-            this.setState({
-                blogItem: response.data.portfolio_blog
-            })
-        })
-        .catch(error => {
-            console.log("getBlogItem error" , error)
-        })
-    }
-
-    componentDidMount(){
-        this.getBlogItem();
-    }
-
-    render() {
-        
-        const{
-            title,
-            content,
-            feature_image_url,
-            blog_status
-
-        }=this.state.blogItem
-        return (
-            <div className='blog-container'>
-                <div className='content-container'>
-                    <h1>{title}</h1>
-
-                    <div className='featured-image-wrapper'> 
-                        <img alt='' src= "feature_image_url" />
-                    </div>
-
-                    <div className='content'>{content}</div>
-                </div>
-            </div>
+          currentId: this.props.match.params.slug,
+          blogItem: {},
+          editMode: false
+        };
+    
+        this.handleEditClick = this.handleEditClick.bind(this);
+        this.handleFeaturedImageDelete = this.handleFeaturedImageDelete.bind(this);
+        this.handleUpdateFormSubmission = this.handleUpdateFormSubmission.bind(
+          this
         );
+      }
+    
+      handleUpdateFormSubmission(blog) {
+        this.setState({
+          blogItem: blog,
+          editMode: false
+        });
+      }
+    
+      handleFeaturedImageDelete() {
+        this.setState({
+          blogItem: {
+            featured_image_url: ""
+          }
+        });
+      }
+    
+      handleEditClick() {
+        if (this.props.loggedInStatus === "LOGGED_IN") {
+          this.setState({ editMode: true });
+        }
+      }
+    
+      getBlogItem() {
+        axios
+          .get(
+            `https://jordan.devcamp.space/portfolio/portfolio_blogs/${
+              this.state.currentId
+            }`
+          )
+          .then(response => {
+            this.setState({
+              blogItem: response.data.portfolio_blog
+            });
+          })
+          .catch(error => {
+            console.log("getBlogItem error", error);
+          });
+      }
+    
+      componentDidMount() {
+        this.getBlogItem();
+      }
+
+      render() {
+        const {
+          title,
+          content,
+          featured_image_url,
+          blog_status
+        } = this.state.blogItem;
+    
+        const contentManager = () => {
+          if (this.state.editMode) {
+            return (
+              <BlogForm
+                handleFeaturedImageDelete={this.handleFeaturedImageDelete}
+                handleUpdateFormSubmission={this.handleUpdateFormSubmission}
+                editMode={this.state.editMode}
+                blog={this.state.blogItem}
+              />
+            );
+          } else {
+            return (
+              <div className="content-container">
+                <h1 onClick={this.handleEditClick}>{title}</h1>
+    
+                <BlogFeaturedImage img={featured_image_url} />
+    
+                <div className='content'>{content}</div>
+              </div>
+            );
+          }
+        };
+    
+        return <div className="blog-container">{contentManager()}</div>;
+      }
     }
-}
+    
 
 export default BlogDetail;
 
